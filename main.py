0988c73d6dea3f43d94f5e0a248d94d9b5fa6d2e
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-09 01:58:31 +0200

    warning for train.txt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from Data_Wrapper import Data
 
+
+# WARNING: It's better to download the ./assets/train.txt from the assignment page since it seem to have problems with crlf thingy on git
 if __name__ == '__main__':
     data = Data("./assets/train.txt")
