4a6d61502bb30c5ca43a628193c456ddd21fad86
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-09 20:02:55 +0200

    Added forward propagation.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,7 +1,10 @@
 import numpy as np
 
+from DataSet import DataSet
+
+
 class NeuralNetwork:
-    def __init__(self, structure, w=None):
+    def __init__(self, structure, f, fDeriv, w=None):
         '''
         constructor
         :param structure: size of each layer, structure[0] is input layer size (including bias), and structure[-1] is output layer size
@@ -9,7 +12,32 @@ class NeuralNetwork:
         self.n = structure
         self.L = len(structure)
         self.a = [np.zeros(shape=length, dtype=np.float64) for length in self.n]
+        self.f_v = np.vectorize(f)
         if (w == None):
-            self.w = [np.zeros(1)] + [np.random.rand(self.n[l], self.n[l - 1]) for l in range(1, self.L)]
+            self.randInitWeights()
         else:
             self.w = w
+
+    def randInitWeights(self):
+        self.w = [np.zeros(1)] + [np.random.rand(self.n[l], self.n[l - 1]) for l in range(1, self.L)]
+
+    def learn(self, ds: DataSet, EPOCHS: int):
+        self.randInitWeights()
+        for i in range(EPOCHS):
+            for [x, y] in ds:
+                self.forward(x)
+                self.backward(y)
+
+    def forward(self, x: np.ndarray):
+        '''
+
+        :param x: 1D input feature vector of size self.n[0]
+        :return: 1D output vector of size self.n[-1]
+        '''
+        self.a[0] = np.copy(x)
+        for i in range (1, self.L):
+            self.a[i] = self.f_v(np.matmul(self.w[i], self.a[i - 1]))
+        return self.a[-1]
+
+    def backward(self, y):
+        pass
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import math
+
 from Data_Wrapper import Data
 from NeuralNetwork import NeuralNetwork
 
 # WARNING: It's better to download the ./assets/train.txt from the assignment page since it seem to have problems with crlf thingy on git
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+def sigmoidDeriv(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
 if __name__ == '__main__':
     data = Data("./assets/train.txt")
 
-    nn = NeuralNetwork(data.structure)
+    nn = NeuralNetwork(data.structure, sigmoid, sigmoidDeriv)
+    nn.learn(data.data, 1000)
