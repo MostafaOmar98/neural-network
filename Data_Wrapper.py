54fa16cf48d9053d6c2a0b62f5a9c165a6f00438
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-09 19:56:06 +0200

    Bugfix.

--- Data_Wrapper.py
+++ Data_Wrapper.py
@@ -14,11 +14,11 @@ class Data:
         structure has the network structure. structure[0] is the size of the input layer (With bias), and structure[-1] is the size of output layer
         '''
         self.structure = pd.read_csv(filepath_or_buffer=path, delim_whitespace=True, header=None, nrows=1).to_numpy(dtype=int).flatten()
-        self.structure[0] += 1
         self.data = pd.read_csv(filepath_or_buffer=path, delim_whitespace=True, header=None, skiprows=[0, 1]).to_numpy(dtype=np.float64)
 
         self.features = self.data[:, :self.structure[0]]
         self.labels = self.data[:, self.structure[0]:]
+        self.structure[0] += 1
         self.inSize = self.features.shape[1] + 1  # Number of features with bias included
         self.outSize = self.labels.shape[1]
         self.data_size = self.features.shape[0]
