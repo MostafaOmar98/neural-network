4067e605502a7c032560caa29b8ebe15602a31f3
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2021-01-09 02:44:11 +0200

    Constructor for nn done.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 class NeuralNetwork:
-    def __init__(self, structure):
+    def __init__(self, structure, w=None):
         '''
         constructor
-        :param structure: size of each layer, structure[0] is input layer size (including bias),
+        :param structure: size of each layer, structure[0] is input layer size (including bias), and structure[-1] is output layer size
         '''
         self.n = structure
-
-
+        self.L = len(structure)
+        self.a = [np.zeros(shape=length, dtype=np.float64) for length in self.n]
+        if (w == None):
+            self.w = [np.zeros(1)] + [np.random.rand(self.n[l], self.n[l - 1]) for l in range(1, self.L)]
+        else:
+            self.w = w
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from Data_Wrapper import Data
-
+from NeuralNetwork import NeuralNetwork
 
 # WARNING: It's better to download the ./assets/train.txt from the assignment page since it seem to have problems with crlf thingy on git
 if __name__ == '__main__':
     data = Data("./assets/train.txt")
+
+    nn = NeuralNetwork(data.structure)
